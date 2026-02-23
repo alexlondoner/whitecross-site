6eed406913835eeda3cca4dfc201216864a961f9
alexlondoner <aerulas@gmail.com>
2026-02-23 00:43:34 +0000

    main change

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("I CUT website is ready!");
     
-    // Form gönderildiğinde ufak bir geri bildirim simülasyonu
     const form = document.querySelector('.booking-form');
     if(form) {
         form.addEventListener('submit', () => {
