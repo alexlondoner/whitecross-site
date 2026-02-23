e042a1b181e5f24a73d552a3ff15b6d355488814
alexlondoner <aerulas@gmail.com>
2026-02-23 02:21:34 +0000

    changess

--- script.js
+++ script.js
@@ -8,24 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
     dateInput.addEventListener('change', () => {
         const selectedDate = new Date(dateInput.value);
         const dayOfWeek = selectedDate.getDay(); 
-
-        timeSelect.innerHTML = '<option value="" disabled selected>Choose a time slot</option>';
+        timeSelect.innerHTML = '<option value="" disabled selected>Time</option>';
 
         let startHour, endHour;
-
-        if (dayOfWeek === 0) { // PAZAR: 10-4
-            startHour = 10; endHour = 16;
-        } else if (dayOfWeek === 6) { // CTS: 9-6
-            startHour = 9; endHour = 18;
-        } else { // HAFTA İÇİ: 9-7
-            startHour = 9; endHour = 19;
-        }
+        if (dayOfWeek === 0) { startHour = 10; endHour = 16; } // Pazar 10-4
+        else if (dayOfWeek === 6) { startHour = 9; endHour = 18; } // Cts 9-6
+        else { startHour = 9; endHour = 19; } // Hafta içi 9-7
 
         for (let hour = startHour; hour < endHour; hour++) {
             ['00', '30'].forEach(min => {
                 const period = hour >= 12 ? 'PM' : 'AM';
-                let displayHour = hour > 12 ? hour - 12 : hour;
-                if (displayHour === 0) displayHour = 12;
+                let displayHour = hour > 12 ? hour - 12 : (hour === 0 ? 12 : hour);
                 const timeText = `${displayHour}:${min} ${period}`;
                 const option = document.createElement('option');
                 option.value = timeText;
