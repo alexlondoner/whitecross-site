98abaae616ac65dff62fefcb1731c6e6c63c6ba2
alexlondoner <108473105+alexlondoner@users.noreply.github.com>
2026-03-25 01:40:15 +0000

    qpsss

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ document.addEventListener('DOMContentLoaded', function () {
             popup.style.display = 'flex';
         }
 
-        fetch("https://script.google.com/macros/s/AKfycbzHWbaSARu_uqHuH1TuWJfa1v2cQx4mY27foWiJ5Jf70kPAFdbtXAiX6MfoUekh1sLZ/exec", {
+        fetch("https://script.google.com/macros/s/AKfycbxFeXvSA85KhEjHzSXU16juhw-OmeEPtWuXC6l89b6jrAU_MinGRCXTd47DkDu9S6tt/exec", {
             method: "POST",
             mode: "no-cors",
             body: JSON.stringify(data)
@@ -250,7 +250,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (pending) {
             const data = JSON.parse(pending);
             data.status = 'CONFIRMED';
-            fetch("https://script.google.com/macros/s/AKfycbzHWbaSARu_uqHuH1TuWJfa1v2cQx4mY27foWiJ5Jf70kPAFdbtXAiX6MfoUekh1sLZ/exec", {
+            fetch("https://script.google.com/macros/s/AKfycbxFeXvSA85KhEjHzSXU16juhw-OmeEPtWuXC6l89b6jrAU_MinGRCXTd47DkDu9S6tt/exec", {
                 method: "POST", mode: "no-cors", body: JSON.stringify(data)
             }).finally(() => sessionStorage.removeItem('pendingBooking'));
         }
