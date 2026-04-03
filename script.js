0231ef1e9247db9d63c2c841dd19c19c81ffaf24
alexlondoner <108473105+alexlondoner@users.noreply.github.com>
2026-04-03 14:51:39 +0100

    upsdate

--- script.js
+++ script.js
@@ -235,7 +235,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             isSubmitting = true; // ← kilitle
 
-            const checkUrl = 'https://script.google.com/macros/s/AKfycbwsPNhaI6hHpZgBhuLgMsi6q54-M1B4DMMGQlsQ4TKvaAdhCCuExDuKXmsdnn_kS-qv/exec?check=duplicate&phone=' + encodeURIComponent(phone) + '&date=' + encodeURIComponent(date);
+            const checkUrl = 'https://script.google.com/macros/s/AKfycbzd1JTYZ96DHkKU3NMInmV9BIUDa5ogEy-ZUXYFTzSAK_sl2iRX7pB7oERHcFXc4cVn/exec?check=duplicate&phone=' + encodeURIComponent(phone) + '&date=' + encodeURIComponent(date);
 
             function handlePayment() {
                 if (extras.includes(service)) {
@@ -287,7 +287,7 @@ document.addEventListener('DOMContentLoaded', function () {
             popup.style.display = 'flex';
         }
 
-        fetch("https://script.google.com/macros/s/AKfycbwsPNhaI6hHpZgBhuLgMsi6q54-M1B4DMMGQlsQ4TKvaAdhCCuExDuKXmsdnn_kS-qv/exec", {
+        fetch("https://script.google.com/macros/s/AKfycbzd1JTYZ96DHkKU3NMInmV9BIUDa5ogEy-ZUXYFTzSAK_sl2iRX7pB7oERHcFXc4cVn/exec", {
             method: "POST",
             mode: "no-cors",
             body: JSON.stringify(data)
@@ -385,7 +385,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        const url = 'https://script.google.com/macros/s/AKfycbwsPNhaI6hHpZgBhuLgMsi6q54-M1B4DMMGQlsQ4TKvaAdhCCuExDuKXmsdnn_kS-qv/exec?date=' + date + '&barber=' + barber;
+        const url = 'https://script.google.com/macros/s/AKfycbzd1JTYZ96DHkKU3NMInmV9BIUDa5ogEy-ZUXYFTzSAK_sl2iRX7pB7oERHcFXc4cVn/exec?date=' + date + '&barber=' + barber;
 
         fetch(url)
             .then(r => r.json())
@@ -490,8 +490,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (bookingData) {
-            bookingData.status = 'CONFIRMED';
-            fetch("https://script.google.com/macros/s/AKfycbwsPNhaI6hHpZgBhuLgMsi6q54-M1B4DMMGQlsQ4TKvaAdhCCuExDuKXmsdnn_kS-qv/exec", {
+            bookingData.status = 'CONFIRMED';   
+            fetch("https://script.google.com/macros/s/AKfycbzd1JTYZ96DHkKU3NMInmV9BIUDa5ogEy-ZUXYFTzSAK_sl2iRX7pB7oERHcFXc4cVn/exec", {
                 method: "POST", mode: "no-cors", body: JSON.stringify(bookingData)
             }).finally(() => sessionStorage.removeItem('pendingBooking'));
         }
