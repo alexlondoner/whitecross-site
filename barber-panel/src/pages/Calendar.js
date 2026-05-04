abd87f93f5ffc15e9e0da66f99fae8eae60799d2
alexlondoner <aerulas@gmail.com>
2026-05-04 22:28:39 +0100

    Build: update for kasa/banka import button

--- barber-panel/src/pages/Calendar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Calendar() {
-  return (
-    <div style={{ color: '#d4af37', fontSize: '1.2rem' }}>
-      Calendar page coming soon...
-    </div>
-  );
-}
